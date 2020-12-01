be77431d200797f221cf312607b4a4482f47083f
Florian Shllaku <florian.shllaku@gmail.com>
2020-12-02 00:48:09 +0200

    Add grid system using bootstrap

---
+++ index.php
@@ -0,0 +1,87 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css\index.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+            <div class="col-md-3 box">
+                <input type="checkbox" name="check-for-delete" value="1" />
+                <div class="inner">
+                    <p><b>SKU</b>: 000-000</p>
+                    <p><b>Name</b>: chair</p>
+                    <p><b>Price</b>: 80$</p>
+                    <p><b>Weight</b>: 5kg</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
