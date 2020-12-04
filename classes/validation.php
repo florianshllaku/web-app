d531b633394c47c0774a77392f3d2bab173aee48
Florian Shllaku <florian.shllaku@gmail.com>
2020-12-04 19:24:11 +0200

    Add validation for user inputs

---
+++ classes/validation.php
@@ -0,0 +1,19 @@
+<?php 
+
+    class Validation{
+
+        public static function numeric_input($integer){
+            if(!is_numeric($integer)){
+                $integer = filter_var($integer, FILTER_SANITIZE_NUMBER_INT);
+            }
+            return $integer;
+        }
+
+        public static function character_input($character){
+            $character = trim(htmlspecialchars($character));
+            return $character;
+        }
+        
+    }
+
+?>
--- insert.php
+++ insert.php
@@ -1,21 +1,22 @@
 <?php
 
 include_once 'includes\autoloader.php';
+include 'classes\validation.php';
 
-        $SKU = $_POST['SKU'];
-        $Name = $_POST['Name'];
-        $Price = $_POST['Price'];
+        $SKU = Validation::character_input($_POST['SKU']);
+        $Name = Validation::character_input($_POST['Name']);
+        $Price = Validation::numeric_input($_POST['Price']);
         $Type = $_POST['Type'];
 
         if($Type == "Books"){
             $id = 1;
-            $Value = $_POST['Weight']." Kg";
+            $Value = Validation::numeric_input($_POST['Weight'])." Kg";
         }else if($Type == "DVDs"){
             $id = 2;
-            $Value = $_POST['Size']." Mb";
-        }else{
+            $Value = Validation::numeric_input($_POST['Size'])." Mb";
+        }else if($Type == "Furniture"){
             $id = 3;
-            $Value = $_POST['Height'] ."cm x ". $_POST['Width'] ."cm x ". $_POST['Length']."cm";
+            $Value = Validation::numeric_input($_POST['Height']) ."cm x ". Validation::numeric_input($_POST['Width']) ."cm x ". Validation::numeric_input($_POST['Length'])."cm";
         }
 
         $add = new Product();
